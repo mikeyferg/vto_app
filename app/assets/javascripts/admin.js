c2d37ddc5d324fdb313c78cb009f2581136c157b
Mike Ferg <mikeyferg@gmail.com>
2016-03-17 17:16:05 -0700

    created admin page

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function(){
+
+
+  $(".customize_submit").on('click', function(){
+       $('body').css("background-color", "red");
+  });
+
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :videos
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  get 'admin' => 'static_pages#admin'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
